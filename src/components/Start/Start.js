aac5b049ce3cbe43d90d557a3050000433cec28e
Emilia Saberski <emilia@saberski.com>
2023-06-21 12:38:23 +0200

    Managed to fix start animation

--- src/components/Start/Start.js
+++ src/components/Start/Start.js
@@ -1,25 +1,59 @@
-import React from 'react'
-import { motion, useScroll, useTransform } from 'framer-motion';
+/* eslint-disable max-len */
+import React from 'react';
+import { motion } from 'framer-motion';
 import './Start.css'
 
 export const Start = () => {
-  const { scrollYProgress } = useScroll();
   // const x1 = useTransform(scrollYProgress, [0, 1], [1000, -1000]);
-  const x2 = useTransform(scrollYProgress, [0, 1], [0, -1000]);
 
   return (
     <div className="start-wrapper">
       <motion.h2
         className="headline-name"
-        initial={{ opacity: 0, x: -200 }}
-        animate={{ opacity: 1, x: 10 }}
+        initial={{ x: '0' }}
+        animate={{ x: '-100%' }}
         transition={{
-          ease: 'easeInOut',
-          duration: 1,
-          delay: 0.4
-        }}>Emilia Saberski
+          duration: 8,
+          repeat: Infinity,
+          repeatType: 'loop',
+          ease: 'linear'
+        }}> Emilia Saberski Emilia Saberski Emilia Saberski
       </motion.h2>
-      <motion.h2 style={{ x2 }} className="headline">Creative Fullstack Developer Motion Designer</motion.h2>
+      <motion.h2
+        className="headline-name"
+        initial={{ x: '100%' }}
+        animate={{ x: '0' }}
+        transition={{
+          duration: 8,
+          repeat: Infinity,
+          repeatType: 'loop',
+          ease: 'linear'
+        }}> Emilia Saberski Emilia Saberski Emilia Saberski
+      </motion.h2>
+      <motion.h2
+        className="headline"
+        initial={{ x: '-100%' }}
+        animate={{ x: '0%' }}
+        // exit={{ x: 1000 }}
+        transition={{
+          duration: 8,
+          repeat: Infinity,
+          repeatType: 'loop',
+          ease: 'linear'
+        }}>Creative Fullstack Developer Motion Designer
+      </motion.h2>
+      <motion.h2
+        className="headline"
+        initial={{ x: '0' }}
+        animate={{ x: '100%' }}
+        // exit={{ x: 1000 }}
+        transition={{
+          duration: 8,
+          repeat: Infinity,
+          repeatType: 'loop',
+          ease: 'linear'
+        }}>Creative Fullstack Developer Motion Designer
+      </motion.h2>
       {/* <h2 className="headline-left">Motion designer</h2> */}
     </div>
   )
