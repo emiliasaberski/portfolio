cf3a5c325112feefb43e02d95cb1a8010fd72d33
Emilia Saberski <emilia@saberski.com>
2023-06-27 09:48:13 +0200

    Added to top button

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import { Route, Routes, BrowserRouter } from 'react-router-dom'
 // We will create these two pages in a moment
 import { Navigation } from 'components/Navigation/Navigation'
+import { ScrollToTop } from 'components/Navigation/ScrollToTop'
 import { HomePage } from './components/Homepage'
 import './index.css'
 
-export const App = () => {
+export const App = (showScrollToTop) => {
+  const refScrollUp = useRef()
+
+  const handleScrollUp = () => {
+    refScrollUp.current.scrollIntoView({ behavior: 'smooth' });
+  };
   return (
     <div className="content-container">
       <BrowserRouter>
+        <ScrollToTop showScrollToTop={showScrollToTop} scrollUp={handleScrollUp} />
         <Navigation class="navigation" />
         <Routes>
           <Route path="/" element={<HomePage />} />
---
+++ src/components/Navigation/ScrollToTop.js
@@ -0,0 +1,28 @@
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+import React, { useState, useEffect } from 'react'
+import './Navigation.css'
+
+export const ScrollToTop = () => {
+  const [showScrollToTop, setShowScrollToTop] = useState(false)
+
+  const handleVisibleButton = () => {
+    setShowScrollToTop(window.pageYOffset > 50)
+  }
+
+  const handleScrollUp = () => {
+    window.scrollTo({ left: 0, top: 0, behavior: 'smooth' })
+  }
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleVisibleButton);
+  }, []);
+
+  return (
+    <div className={showScrollToTop ? '' : 'scrollToTopHidden'} onClick={handleScrollUp}>
+      <button type="button" className="scrollToTop">
+           ^ To Top ^
+      </button>
+    </div>
+  );
+};
