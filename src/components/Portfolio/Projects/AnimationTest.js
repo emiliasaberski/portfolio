104e1088d654b591f9a9e42b289992d4d56202e7
Emilia Saberski <emilia@saberski.com>
2024-02-13 14:15:43 +0100

    stuff

---
+++ src/components/Portfolio/Projects/AnimationTest.js
@@ -0,0 +1,53 @@
+import gsap from 'gsap';
+import ScrollTrigger from 'gsap/ScrollTrigger';
+import React, { useLayoutEffect, useRef } from 'react';
+import './Animation.css';
+
+gsap.registerPlugin(ScrollTrigger);
+
+export const AnimationTest = () => {
+  const component = useRef();
+  const slider = useRef();
+
+  useLayoutEffect(() => {
+    const ctx = gsap.context(() => {
+      const panels = gsap.utils.toArray('.panel');
+      gsap.to(panels, {
+        xPercent: -100 * (panels.length - 1),
+        ease: 'none',
+        scrollTrigger: {
+          trigger: slider.current,
+          pin: true,
+          scrub: 1,
+          snap: 1 / (panels.length - 1),
+          end: () => `+=${slider.current.offsetWidth}`,
+          markers: true
+        }
+      });
+    }, component);
+    return () => ctx.revert();
+  });
+
+  return (
+    <div className="App" ref={component}>
+      <div className="firstContainer">
+        <h1>Testing horizontal scrolling w/ three sections</h1>
+        <h2>First Container</h2>
+      </div>
+      <div ref={slider} className="container">
+        <div className="description panel blue">
+          <div>
+            SCROLL DOWN
+            <div className="scroll-down">
+              <div className="arrow" />
+            </div>
+          </div>
+        </div>
+        <div className="panel red">ONE</div>
+        <div className="panel orange">TWO</div>
+        <div className="panel purple">THREE</div>
+      </div>
+      <div className="lastContainer">Last Container</div>
+    </div>
+  );
+}
